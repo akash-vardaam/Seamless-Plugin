0fa2454aef6221464322c60ba360e3e5cc3e2fc5
akash-vardaam <avasava@vardaam.email>
2026-03-24 17:59:47 +0530

    refactor: remove legacy public PHP/JS rendering and use React mounts

--- Seamless_Plugin-main/seamless-20260211-190730/seamless-wordpress-plugin/src/Public/templates/tpl-single-event-wrapper.php
+++ Seamless_Plugin-main/seamless-20260211-190730/seamless-wordpress-plugin/src/Public/templates/tpl-single-event-wrapper.php
@@ -1,6 +1,5 @@
-<?php get_header();
+<?php
 $event_slug = get_query_var('event_uuid');
-$event_type = isset($_GET['type']) ? sanitize_text_field($_GET['type']) : 'event';
-?>
-<?php echo do_shortcode('[seamless_single_event slug="' . esc_attr($event_slug) . '" type="' . esc_attr($event_type) . '"]'); ?>
-<?php get_footer(); ?>
+$event_type = get_query_var('event_type') ?: 'event';
+
+echo do_shortcode('[seamless_single_event slug="' . esc_attr($event_slug) . '" type="' . esc_attr($event_type) . '"]');
